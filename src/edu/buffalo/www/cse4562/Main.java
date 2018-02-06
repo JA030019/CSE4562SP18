f41c4139ca468991e191ba18a105bd293053cbb2
Zheng Kai <kaizheng1993@gmail.com>
2018-02-06 14:58:25 -0500

    change Main.java

--- src/edu/buffalo/www/cse4562/Main.java
+++ src/edu/buffalo/www/cse4562/Main.java
@@ -1,5 +1,10 @@
-public class Main{
-    public static void main(String[] args){
-        System.out.println("Hello,World");
-    }
+package edu.buffalo.www.cse4562;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello, World");
+	}
+
 }
