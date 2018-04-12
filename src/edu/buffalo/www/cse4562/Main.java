5f2cf4322f8c85018d03031c9d4b972aab997e5d
Zheng Kai <kaizheng1993@gmail.com>
2018-04-12 15:14:37 -0400

    checkpoint 3

--- src/edu/buffalo/www/cse4562/Main.java
+++ src/edu/buffalo/www/cse4562/Main.java
@@ -475,7 +475,7 @@ public class Main {
 			  //case4 groupby function		  
 			  else{
 				  
-				  ap = new AggregationOperator3(so,expHaving, columnRefList, expProList, op); 
+				  ap = new AggregationOperator(so,expHaving, columnRefList, expProList, op); 
 			  } 
   
 			  /*
